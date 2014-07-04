9ebfa569ec3446f96507a84b06ec3a0d52a96a9e
Guillermo Esteves <gfesteves@gmail.com>
2014-07-03 22:44:03 -0400

    Use redis pipeline

--- app.rb
+++ app.rb
@@ -44,12 +44,14 @@ def store_markov(text)
   words = text.split(/\s+/)
   # Ignore if phrase is less than 3 words
   unless words.size < 3
-    (words.size - 2).times do |i|
-      # Join the first two words as the key
-      key = words[i..i+1].join(' ')
-      # And the third as a value
-      value = words[i+2]
-      $redis.sadd(key, value)
+    $redis.pipelined do
+      (words.size - 2).times do |i|
+        # Join the first two words as the key
+        key = words[i..i+1].join(' ')
+        # And the third as a value
+        value = words[i+2]
+        $redis.sadd(key, value)
+      end
     end
   end
 end
