4923eb9bbb7101f44e4574c0425273f98b6e0112
Guillermo Esteves <g@gesteves.com>
2015-02-03 20:20:33 -0500

    Don't tweet if from the webform

--- app.rb
+++ app.rb
@@ -76,7 +76,7 @@ post "/markov" do
     end
 
     # Reply if the bot isn't shushed AND either the random number is under the threshold OR the bot was invoked
-    if !$redis.exists("bot:shush") && (SecureRandom.random_number <= ENV["RESPONSE_CHANCE"].to_f || params[:text].match(settings.reply_to_regex))
+    if !$redis.exists("bot:shush") && params[:user_id] != "WEBFORM" && (SecureRandom.random_number <= ENV["RESPONSE_CHANCE"].to_f || params[:text].match(settings.reply_to_regex))
       reply = build_markov
       response = json_response_for_slack(reply)
       tweet(reply) unless ENV["SEND_TWEETS"].nil? || ENV["SEND_TWEETS"].downcase == "false"
