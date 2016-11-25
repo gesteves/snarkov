eb5492b96ddbaa16a0a844b2a73e4f6b7d6c9734
Guillermo Esteves <guillermo@voxmedia.com>
2016-11-25 07:59:29 -0500

    Increase caching for some API calls

--- app.rb
+++ app.rb
@@ -251,7 +251,7 @@ def get_slack_name(slack_id)
       name = ''
       puts "[ERROR] fetching user: #{response['error']}" unless response['error'].nil?
     end
-    $memcached.set(cache_key, name, 60 * 60 * 24)
+    $memcached.set(cache_key, name, 60 * 60 * 24 * 30)
   end
   name
 end
@@ -266,7 +266,7 @@ def get_slack_user_id(username)
       users = JSON.parse(users_list)['members']
       user = users['members'].find { |u| u['name'] == username.downcase }
       user_id = user['id'] unless user.nil?
-      $memcached.set(cache_key, user_id, 60 * 60 * 24)
+      $memcached.set(cache_key, user_id, 60 * 60 * 24 * 365)
     end
   end
 
@@ -300,7 +300,7 @@ def get_channel_id(channel_name)
       channels = JSON.parse(channels_list)['channels']
       channel = channels.find { |c| c['name'] == channel_name.gsub('#','') }
       channel_id = channel['id'] unless channel.nil?
-      $memcached.set(cache_key, channel_id, 60 * 60 * 24)
+      $memcached.set(cache_key, channel_id, 60 * 60 * 24 * 365)
     end
   end
 
@@ -343,7 +343,7 @@ def get_channel_name(channel_id)
     else
       channel_name = "##{channel.last}"
     end
-    $memcached.set(cache_key, channel_name, 60 * 60 * 24)
+    $memcached.set(cache_key, channel_name, 60 * 60 * 24 * 30)
   end
 
   channel_name
